c2ea206afcfd9e2b7d3cc19cd83a473eeb698eb9
arronei <arronei@microsoft.com>
2016-08-25 15:56:11 -0700

    Initial check-in of FastDOM compiler

---
+++ FastDOMCompiler/FastDOMCompiler/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MS.Internal
+{
+    /// <summary>Root class for the application</summary>
+    public class FastDomCompiler
+    {
+        /// <summary>Main method for the application, and app entry point</summary>
+        /// <param name="args">Input arguments for the app</param>
+        public static void Main(string[] args)
+        {
+            var outputDirectory = string.Empty;
+            var hdlDirectory = string.Empty;
+            var validateDirectory = string.Empty;
+            var buildEnvironment = string.Empty;
+        }
+    }
+}
