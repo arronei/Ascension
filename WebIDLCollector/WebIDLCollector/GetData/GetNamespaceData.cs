90262c9a556d56d7f09b3b6ec018ffa63c920bac
arronei <arronei@microsoft.com>
2016-09-28 16:19:21 -0700

    Name space files

---
+++ WebIDLCollector/WebIDLCollector/GetData/GetNamespaceData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WebIDLCollector.IDLTypes;
+
+namespace WebIDLCollector.GetData
+{
+    public partial class DataCollectors
+    {
+        private static readonly Regex NamespaceParser = new Regex(@"(\[(?<extended>[^\]]+)\]\s*)?
+        ((?<partial>partial)\s+)?(/\*[^\*]+\*/\s*)?namespace\s+(/\*[^\*]+\*/\s*)?
+        (?<name>\w+)\s*\{\s*(?<members>([^\}]*?\{.*?\};.*?|[^\}]+))?\s*\};?", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
+
+        private static readonly Regex NamespaceExtendedParser = new Regex(@"((exposed(\s*=\s*(?<exposed>(\([^\)]+\))|[^\(\s,\]]+))?)(,|$)|
+        (?<securecontext>securecontext)(,|$))+", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
+
+        private static readonly Regex NamespaceMember = new Regex(@"^\s*(\[(?<extended>[^\]]+)]\s*)?
+        (?<type>.+)\s+(?<item>[^\(\s]+)?\s*(?<function>\((?<args>.*)\))$", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
+
+        private static readonly Regex NamespaceMemberExtendedParser = new Regex(@"((exposed(\s*=\s*(?<exposed>(\([^\)]+\))|[^\(\s,\]]+))?)(,|$)|
+        (?<securecontext>securecontext)(,|$))+", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
+
+        public static IEnumerable<NamespaceType> GetAllNamespaces(string cleanString, SpecData specificationData)
+        {
+            var namespaces = new List<NamespaceType>();
+
+            foreach (Match _namespace in NamespaceParser.Matches(cleanString))
+            {
+
+            }
+
+            return namespaces;
+        }
+    }
+}
---
+++ WebIDLCollector/WebIDLCollector/IDLTypes/NamespaceType.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebIDLCollector.IDLTypes
+{
+    public class NamespaceType
+    {
+        public NamespaceType()
+        {
+            Exposed = new List<string>();
+        }
+
+        public IEnumerable<string> Exposed { get; set; }
+
+        public bool SecureContext { get; set; }
+        public bool IsPartial { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Member> Members { get; set; }
+        public IEnumerable<string> SpecNames { get; set; }
+
+        public string Reconstruct(bool showMemberSpecName = false)
+        {
+            var sb = new StringBuilder();
+            var comma = string.Empty;
+
+            sb.AppendLine("// " + string.Join(", ", SpecNames));
+
+            if (Exposed.Any() ||
+                SecureContext)
+            {
+                sb.Append("[");
+                if (Exposed.Any())
+                {
+                    sb.Append(comma).Append("Exposed=");
+
+                    if (Exposed.Count() == 1)
+                    {
+                        sb.Append(Exposed.Single());
+                    }
+                    else
+                    {
+                        sb.Append("(").Append(string.Join(", ", Exposed)).Append(")");
+                    }
+                    comma = ", ";
+                }
+                if (SecureContext)
+                {
+                    sb.Append(comma).Append("SecureContext");
+                    comma = ", ";
+                }
+                sb.AppendLine("]");
+            }
+            if (IsPartial)
+            {
+                sb.Append("partial ");
+            }
+            sb.Append("namespace ");
+            sb.Append(Name);
+            if (!Members.Any())
+            {
+                sb.AppendLine(" { };");
+            }
+            else
+            {
+                sb.AppendLine().AppendLine("{");
+
+                foreach (var member in Members)
+                {
+                    sb.Append("    ").AppendLine(member.Reconstruct(showMemberSpecName));
+                }
+
+                sb.AppendLine("};");
+            }
+
+            return sb.ToString();
+        }
+
+        public class NamespaceNameCompare : IEqualityComparer<NamespaceType>
+        {
+            public bool Equals(NamespaceType x, NamespaceType y)
+            {
+                return x.Name.Equals(y.Name);
+            }
+
+            public int GetHashCode(NamespaceType obj)
+            {
+                return obj.Name.GetHashCode();
+            }
+        }
+    }
+}
