79bc522ebcb9f62cb0906fb423cc2b57c511193d
Arron Eicholz <arronei@microsoft.com>
2017-04-17 08:54:03 -0700

    Initial commit of WebIDL downloader

---
+++ WebIDLDownloader/WebIDLDownloader/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace WebIDLDownloader
+{
+    public static class Program
+    {
+        private const string WebidlLocation = "webidl";
+
+        public static void Main(string[] args)
+        {
+            if (Directory.Exists(WebidlLocation))
+            {
+                Console.WriteLine("Deleting old files...");
+                Directory.Delete(WebidlLocation, true);
+            }
+            DownloadAllWebIdls();
+
+            Console.WriteLine("Download Complete!");
+            Console.ReadLine();
+        }
+
+        private static void DownloadAllWebIdls()
+        {
+            DownloadBlinkWebIdl();
+            //DownloadGeckoWebIdl();
+            //DownloadWebkitWebIdl();
+            //DownloadEdgeWebIdl();
+        }
+
+        private static void DownloadBlinkWebIdl()
+        {
+            Console.WriteLine("Downloading Blink WebIDL files");
+
+            PullLatestFiles(WebPlatformId.Blink);
+
+            Console.WriteLine("Blink WebIDL file download complete");
+        }
+
+        private static void DownloadGeckoWebIdl()
+        {
+            Console.WriteLine("Downloading Gecko WebIDL files");
+
+            PullLatestFiles(WebPlatformId.Gecko);
+
+            Console.WriteLine("Gecko WebIDL file download complete");
+        }
+
+        private static void DownloadWebkitWebIdl()
+        {
+            Console.WriteLine("Downloading Webkit WebIDL files");
+
+            PullLatestFiles(WebPlatformId.Webkit);
+
+            Console.WriteLine("Webkit WebIDL file download complete");
+        }
+
+        private static void DownloadEdgeWebIdl()
+        {
+            Console.WriteLine("Downloading Edge WebIDL files");
+
+            //Copy files over
+
+            Console.WriteLine("Edge WebIDL file download complete");
+        }
+
+        private static void PullLatestFiles(WebPlatformId platform)
+        {
+            var process = new Process();
+            var startInfo = new ProcessStartInfo
+            {
+                //WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                WorkingDirectory = @"D:\GitHub\Browsers\" + platform,
+                FileName = "cmd.exe",
+                Arguments = "/C git pull --ff-only"
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+
+    internal enum WebPlatformId
+    {
+        Blink,
+        Gecko,
+        Webkit,
+        Edge
+    }
+}
