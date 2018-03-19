34f6c68dae9ff9a492574bc7f5031ed1d7e56cdf
Arron Eicholz <arronei@microsoft.com>
2018-03-19 13:27:45 -0700

    System.IO.Csv initial implementation

---
+++ System.IO.Csv/System.IO.Csv/ValueTrimmingOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace System.IO.Csv
+{
+    [Flags]
+    public enum ValueTrimmingOptions
+    {
+        None = 0,
+        UnquotedOnly = 1,
+        QuotedOnly = 2,
+        All = UnquotedOnly | QuotedOnly
+    }
+}
